e7f16dc2896fbc2ff9106be68e2ea84567bd6987
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-20 17:40:49 +0000

    get journey start location passed

--- models/traveller.js
+++ models/traveller.js
@@ -2,30 +2,34 @@ const Traveller = function(journeys) {
   this.journeys = journeys;
 };
 
-Traveller.prototype.getJourneyStartLocations = function(location) {
-  const startLocation = this.journey.filter(location => location.startLocation === location);
-  return startLocation;
+Traveller.prototype.getJourneyStartLocations = function (location) {
+  return this.journeys.map(function (journey){
+    return journey.startLocation;
+  })
 };
 
-Traveller.prototype.getJourneyEndLocations = function () {
 
-};
-
-Traveller.prototype.getJourneysByTransport = function (transport) {
-
-};
-
-Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
-};
-
-Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
-};
-
-Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
+// Traveller.prototype.getJourneyEndLocations = function () {
+//   return journeys.map((journey) => {
+//     return journeys.endLocation;
+//   });
+// };
+//
+// Traveller.prototype.getJourneysByTransport = function (transport) {
+//
+// };
+//
+// Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+//
+// };
+//
+// Traveller.prototype.calculateTotalDistanceTravelled = function () {
+//
+// };
+//
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//
+// };
 
 
 module.exports = Traveller;
